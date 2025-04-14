33158da4d13ee7d631d790c8bf7fc7752c02c3b8
алина лэнгли <alinalengli@alinas-MacBook-Air-2.local>
2025-04-14 11:21:12 +0600

    Added recursive backtracking for maze generation

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+def create_empty_maze(rows, cols):
+    return [['#' for _ in range(cols)] for _ in range(rows)]
+import random
+
+DIRS = [(-2, 0), (2, 0), (0, -2), (0, 2)]
+
+def generate_maze(maze, r, c):
+    maze[r][c] = ' '
+    random.shuffle(DIRS)
+    for dr, dc in DIRS:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < len(maze) and 0 <= nc < len(maze[0]) and maze[nr][nc] == '#':
+            wall_r, wall_c = r + dr // 2, c + dc // 2
+            maze[wall_r][wall_c] = ' '
+            generate_maze(maze, nr, nc)
